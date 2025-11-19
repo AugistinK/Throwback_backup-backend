54cd7b28cbb2a9c695ea272d34216a4c340ae066
AugistinK <g.nadieline@infinitiax.com>
2025-11-19 13:17:38 +0100

    Commit 133

--- routes/supportRoutes.js
+++ routes/supportRoutes.js
@@ -2,9 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const { contactSupport } = require('../controllers/supportController');
-const { protect } = require('../middlewares/authMiddleware'); 
+const { protect } = require('../middlewares/authMiddleware');
 
-// POST /api/support/contact
 router.post('/contact', protect, contactSupport);
 
 module.exports = router;
